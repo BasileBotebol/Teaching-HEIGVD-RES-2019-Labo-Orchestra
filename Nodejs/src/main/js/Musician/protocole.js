2cc643cb263ad21b8e2cf8a29ccb147b4e1fb3e7
BasileBotebol <basile.botebol@gmail.com>
2019-05-13 08:44:29 +0200

    deleted obsolete files

--- Nodejs/src/main/js/Musician/protocole.js
+++
@@ -1,9 +0,0 @@
-exports.PROTOCOL_MULTICAST_ADDRESS = "127.0.0.1";
-exports.PROTOCOL_PORT = 2205;
-exports.soundByInstrument = {
-    "piano"  : "ti-ta-ti",
-    "trumpet": "pouet",
-    "flute"  : "trulu",
-    "violin" : "gzi-gzi",
-    "drum"   : "boum-boum"
-};
